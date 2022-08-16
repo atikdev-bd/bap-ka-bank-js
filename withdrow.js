d185037ad5772af9b21c55415175ce412a86e54f
atikdev-bd <atikdev.bd@gmail.com>
2022-08-16 16:21:47 +0600

    modifie withdrow section

--- withdrow.js
+++ withdrow.js
@@ -1,24 +1,30 @@
-const withdrow = document.getElementById('withdrow-btn').addEventListener('click', function () {
-    const withdrowInput = document.getElementById('withdrow-input');
-    const withdrowValueString = withdrowInput.value;
-    const withdrowValue = parseFloat(withdrowValueString);
-    withdrowInput.value = '';
-
+/// add enventlistener in withdrow button \\\
+document.getElementById('withdrow-btn').addEventListener('click', function () {
+    const withdrow = document.getElementById('withdrow-input');
+    const withdrowString = withdrow.value;
+    const withdrowAmount = parseFloat(withdrowString);
+    withdrow.value = '';
+    // cheak new withdrow amount valid or not \\\\
+    if (isNaN(withdrowAmount)) {
+        alert('please provide valid number')
+        return;
+    }
+    /// calculate previous withdrow balance + new withdrow balance \\\ 
     const withdrowTotal = document.getElementById('withdrow-total');
-    const withdrowBalance = withdrowTotal.innerText;
-    const withdrowTotalBalance = parseFloat(withdrowBalance)
+    const previousWithdrowString = withdrowTotal.innerText;
+    const withdrowTotalAmount = parseFloat(previousWithdrowString);
 
-    const totalWithdrowBalance = withdrowTotalBalance + withdrowValue;
-
-    withdrowTotal.innerText = totalWithdrowBalance;
-
-    const balance = document.getElementById('balance-total');
-    const balanceInput = balance.innertext;
-    const totalBalance = parseFloat(balanceInput) ;
-    // const newBalanceTotal = totalBalance - ;
-    // balance.innertext = newBalanceTotal;
-    console.log(totalBalance)
-
-
-
+    const totalBalance = document.getElementById('balance-total');
+    const totalBalanceString = totalBalance.innerText;
+    const totalBalanceAmount = parseFloat(totalBalanceString);
+    /// cheak withdrow amount === tatal balance  \\\
+    if (withdrowAmount > totalBalanceAmount) {
+        alert('Tera bap ka bank mye paisa nehi he')
+        return;
+    }
+    const currentWithdroewTotal = withdrowTotalAmount + withdrowAmount;
+    withdrowTotal.innerText = currentWithdroewTotal;
+    /// calculate total balance - new withdrow amount
+    const newBalanceTotal = totalBalanceAmount - withdrowAmount;
+    totalBalance.innerText = newBalanceTotal;
 })
